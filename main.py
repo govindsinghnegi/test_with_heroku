3275737088f973114832b1fed18b9d0a5501210d
gsn <gsn@example.com>
2022-08-14 23:30:20 -0700

    add Procfile, change main and requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 def create_df_from_dict():
     data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
     return pd.DataFrame.from_dict(data)
 
-if __name__ == "__main__":
-    df = create_df_from_dict()
-    print(df.shape)
+app = FastAPI()
+
+class Value(BaseModel):
+    value: int
+
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello World!"}
+
+@app.post("/{path}")
+async def exercise_function(path: int, query: int, body: Value):
+    return {"path": path, "query": query, "body": body}
